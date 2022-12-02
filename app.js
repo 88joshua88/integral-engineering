a0097a65f88915980eee1ad75d3cf46f89792435
88joshua88 <66178079+88joshua88@users.noreply.github.com>
2022-12-02 12:57:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("express");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+})); //converts posted html form data to usable format
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/homePic1.png");
+});
+
+
+
+app.post('/', (req, res) => {
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  console.log(firstName, lastName, email);
+
+
+});
+
+
+app.post('/failure', (req, res) => {
+
+  res.redirect("/");
+})
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
+
+
+// 0e51ffb90db281f47e3f93b70abb8520-us21  apikey
+// e4f4e01689  list id
